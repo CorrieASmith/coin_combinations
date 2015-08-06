adb4cff19aad704f82fa47866b37b66eba5a8dfe
ronando & Corrie Smith <student@epicodus.com>
2015-08-06 12:09:30 -0700

    coin passes second spec

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -1,6 +1,13 @@
 class Fixnum
     define_method(:change) do
-      var_a = self
-      var_a = var_a./(25).floor()
-    end
+      input = self
+      coins =  [0, 0, 0, 0]
+      remainder = input.%(25)  # R: 16 w/ 166 input
+
+      coins.shift
+      coins.unshift(quarters = input./(25))      # quarters
+      coins.delete_at(1)
+      coins.insert(1, (dimes = remainder./(10)))     # dimes
+      coins
+  end
 end
--- spec/coin_combination_spec.rb
+++ spec/coin_combination_spec.rb
@@ -2,7 +2,17 @@ require('rspec')
 require('coin_combinations')
 
 describe('Fixnum#change') do
-  it('receives user input and divides by four') do
-    expect(166.change).to(eq(6))
+
+  it('receives user input and divides quarters') do
+    expect(166.change).to(eq([6, 1, 0, 0]))
   end
+
+  it('takes remainder after quarters, then divides by dimes') do
+    expect(166.change).to(eq([6, 1, 0, 0]))
+  end
+
+  it('takes remainder after dimes, then divides by nickels') do
+    expect(166.change).to(eq([6, 1, 1, 0]))
+  end
+
 end
